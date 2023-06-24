41a9f84b3a2df54a4460b5a8358ab64bf017f803
mratri10 <atriariskaalfa10@gmail.com>
2023-06-25 06:01:30 +0700

    Success SetUp Database

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+require('dotenv').config()
+
+const {
+  DB_HOSTNAME,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_PORT,
+  DB_DIALECT,
+} = process.env
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource sad' + process.env.DB_PORT);
 });
 
 module.exports = router;
